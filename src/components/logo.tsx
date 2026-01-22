7dd86894b5c96121ed4126abb601c9f6e8149db7
Emmanuel Solomon <xostip@gmail.com>
2026-01-22 21:29:53 +0000

    logo is too small make it 4 times bigger (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
     <Image 
       src="https://i.postimg.cc/LsTgHt8c/Chat-GPT-Image-Jan-22-2026-10-17-26-PM.png" 
       alt="Nightingale Apartments Logo"
-      width={50} 
-      height={50}
+      width={100} 
+      height={100}
       className="rounded-md invert"
     />
   );
