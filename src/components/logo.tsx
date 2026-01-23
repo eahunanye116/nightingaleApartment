faed0bf529131049d9450207685978e9db14e4a8
Emmanuel Solomon <xostip@gmail.com>
2026-01-23 10:19:00 +0000

    i said use text for logo just write out Nightingale Apartment

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image';
-
 export function Logo() {
   return (
-    <Image
-      src="https://i.postimg.cc/y604MvVx/logo.png"
-      alt="Nightingale Logo"
-      width={150}
-      height={40}
-      className="object-contain brightness-0 invert"
-    />
+    <span className="font-headline text-2xl font-bold text-white">
+      Nightingale Apartment
+    </span>
   );
 }
