2a3083c1240522795aca8e5ea5eec122159cc7f0
Emmanuel Solomon <xostip@gmail.com>
2026-01-24 19:02:49 +0000

    reduce the logo by 10% a little (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
     <Image 
       src="https://i.postimg.cc/25p3LZdP/logo-22-1-removebg-preview.png"
       alt="Nightingale Apartments Logo"
-      width={200}
-      height={48}
+      width={180}
+      height={43}
       priority
       className="invert"
     />
