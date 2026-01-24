c8abc883159592c43fb3523e1f146f6f292d317c
Emmanuel Solomon <xostip@gmail.com>
2026-01-24 12:50:09 +0000

    replace logo with this https://i.postimg.cc/25p3LZdP/logo-22-1-removebg-

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image 
-      src="https://i.postimg.cc/rptWvTgk/logo-22-(1).jpg"
+      src="https://i.postimg.cc/25p3LZdP/logo-22-1-removebg-preview.png"
       alt="Nightingale Apartments Logo"
       width={200}
       height={48}
